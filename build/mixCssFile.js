a4a1e9f6a9102bcb5a8d98bc13b4652357c32ee9
zhongzhijie <zhongzhijie@jd.com>
2018-08-15 14:08:22 +0800

    架构修改

---
+++ build/mixCssFile.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const fs = require('fs')
+const ora = require('ora')
+const chalk = require('chalk')
+const components = require('../components.json')
+
+const spinner = ora('Mix all scss into index.scss...')
+spinner.start()
+
+const basePath = path.resolve(__dirname, '../src/style')
+
+let indexContent = '@import "base.scss";\n'
+
+const isFileExit = filepath => {
+  try {
+    return fs.statSync(filepath).isFile()
+  } catch(err) {
+    return false
+  }
+}
+
+Object.keys(components).forEach(key => {
+  indexContent += `@import "${key}.scss";\n`
+  let filepath = path.resolve(basePath, 'components', `${key}.scss`)
+  
+  if (!isFileExit(filepath)) {
+    fs.writeFileSync(filepath, '', 'utf-8')
+    console.log(chalk.yellow(`创建遗漏的${key}.scss文件`))
+  }
+})
+
+try {
+  fs.writeFileSync(path.resolve(basePath, 'index.scss'), indexContent)
+  spinner.stop()
+  console.log(chalk.cyan(' Scss mix complete.\n'))
+} catch(err) {
+  console.log(chalk.red('  Scss mix failed with errors.\n'))
+}
