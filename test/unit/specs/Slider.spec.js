879262b925b016f6105ee32c408003efacfea8d8
changsijie <changsijie@jd.com>
2018-09-28 17:31:57 +0800

    完成测试用例

--- test/unit/specs/Slider.spec.js
+++ test/unit/specs/Slider.spec.js
@@ -12,77 +12,99 @@ describe('Slider', () => {
       template: '<jm-slider></jm-slider>'
     })
 
-    vm.$el.querySelector('.jm-slider__label-min').textContent.should.equal('0')
-    vm.$el.querySelector('.jm-slider__label-max').textContent.should.equal('100')
-    vm.$el.querySelector('.jm-slider__label-cur').textContent.should.equal('0')
+    expect(vm.$el.querySelector('.jm-slider__label-min').textContent).to.equal('0')
+    expect(vm.$el.querySelector('.jm-slider__label-max').textContent).to.equal('100')
+    expect(vm.$el.querySelector('.jm-slider__label-cur').textContent).to.equal('0')
   })
 
-  it('should render correct contents when max and min is set', () => {
+  it('should render correct contents when max and min is set', done => {
     vm = createTplVM({
       template: '<jm-slider :maxValue="666" :minValue="233"></jm-slider>'
     })
-
-    vm.$el.querySelector('.jm-slider__label-min').textContent.should.equal('233')
-    vm.$el.querySelector('.jm-slider__label-max').textContent.should.equal('666')
-    vm.$el.querySelector('.jm-slider__label-cur').textContent.should.equal('233')
+    
+    vm.$nextTick(() => {
+      expect(vm.$el.querySelector('.jm-slider__label-min').textContent).to.equal('233')
+      expect(vm.$el.querySelector('.jm-slider__label-max').textContent).to.equal('666')
+      expect(vm.$el.querySelector('.jm-slider__label-cur').textContent).to.equal('233')
+      done()
+    })
   })
 
-  it('should render single slider when type="single" is set', () => {
+  it('should render single slider when type="single" is set', done => {
     vm = createTplVM({
       template: '<jm-slider type="single"></jm-slider>'
     })
 
     const $labelCur = vm.$el.querySelectorAll('.jm-slider__label-cur')
 
-    vm.$el.querySelectorAll('.jm-slider__ball-container').should.have.lengthOf(1)
-    vm.$el.querySelectorAll('.jm-slider__ball').should.have.lengthOf(1)
-    $labelCur.should.have.lengthOf(1)
-    $labelCur[0].textContent.should.equal('0')
+    vm.$nextTick(() => {
+      expect(vm.$el.querySelectorAll('.jm-slider__ball-container')).to.have.length(1)
+      expect(vm.$el.querySelectorAll('.jm-slider__ball')).to.have.length(1)
+      expect($labelCur).to.have.length(1)
+      expect($labelCur[0].textContent).to.equal('0')
+      done()
+    })
   })
 
-  it('should render double slider when type="double" is set', () => {
+  it('should render double slider when type="double" is set', done => {
     vm = createTplVM({
       template: '<jm-slider type="double"></jm-slider>'
     })
 
-    const $labelCur = vm.$el.querySelectorAll('.jm-slider__label-cur') 
+    const $labelCur = vm.$el.querySelectorAll('.jm-slider__label-cur')
 
-    vm.$el.querySelectorAll('.jm-slider__ball-container').should.have.lengthOf(2)
-    vm.$el.querySelectorAll('.jm-slider__ball').should.have.lengthOf(2)
-    $labelCur.should.have.lengthOf(2)
-    $labelCur[0].textContent.should.equal('0')
-    $labelCur[1].textContent.should.equal('100')
+    vm.$nextTick(() => {
+      expect(vm.$el.querySelectorAll('.jm-slider__ball-container')).to.have.length(2)
+      expect(vm.$el.querySelectorAll('.jm-slider__ball')).to.have.length(2)
+      expect($labelCur).to.have.length(2)
+      expect($labelCur[0].textContent).to.equal('0')
+      expect($labelCur[1].textContent).to.equal('100')
+      done()
+    })
+  
   })
 
-  it('should render correct content when single value is set', () => {
+  it('should render correct content when single value is set', done => {
     vm = createTplVM({
       template: '<jm-slider value="33" ref="slider"></jm-slider>'
     })
 
     const $labelCur = vm.$el.querySelector('.jm-slider__label-cur')
 
-    $labelCur.should.equal('33')
-    vm.$refs.slider.setValue(-30)
-    $labelCur.should.equal('0')
-    vm.$refs.slider.setValue(130)
-    $labelCur.should.equal('100')
+    vm.$nextTick(() => {
+      expect($labelCur.textContent).to.equal('33')
+      vm.$refs.slider.setValue(-30)
+      vm.$nextTick(() => {
+        expect($labelCur.textContent).to.equal('0')
+        vm.$refs.slider.setValue(130)
+        vm.$nextTick(() => {
+          expect($labelCur.textContent).to.equal('100')
+          done()
+        })
+      })
+    })
   })
 
-  it('should render correct content when double value is set', () => {
+  it('should render correct content when double value is set', done => {
     vm = createTplVM({
       template: '<jm-slider type="double" value="33,77" ref="slider"></jm-slider>'
     })
 
     const $labelCur = vm.$el.querySelectorAll('.jm-slider__label-cur')
 
-    $labelCur[0].should.equal('33')
-    $labelCur[1].should.equal('77')
-    vm.$refs.slider.setValue(-30, 130)
-    $labelCur[0].should.equal('0')
-    $labelCur[1].should.equal('100')
+    vm.$nextTick(() => {
+      $labelCur[0].textContent.should.equal('33')
+      $labelCur[1].textContent.should.equal('77')
+      vm.$refs.slider.setValue(-30, 130)
+      vm.$nextTick(() => {
+        $labelCur[0].textContent.should.equal('0')
+        $labelCur[1].textContent.should.equal('100')
+        done()
+      })
+    })
   })
 
-  it('should get the correct value when using method getValue()', () => {
+  it('should get the correct value when using method getValue', () => {
     vm = createTplVM({
       template: '<jm-slider value="33" ref="slider"></jm-slider>'
     })
