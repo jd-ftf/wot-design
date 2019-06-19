b3daf059116d254344045686a33dfc61869d6cab
zhongzhijie <zhongzhijie@jd.com>
2019-06-19 22:22:55 +0800

    输入框

--- examples-m/pages/input.vue
+++ examples-m/pages/input.vue
@@ -5,16 +5,32 @@
       <jm-input v-model="input1" placeholder="请输入用户名" />
     </white-space>
     <white-space>
-      <div slot="desc">禁用状态</div>
+      <div slot="desc">禁用状态 <code>disabled</code></div>
       <jm-input v-model="input2" disabled />
     </white-space>
     <white-space>
-      <div slot="desc">只读状态</div>
+      <div slot="desc">只读状态 <code>readonly</code></div>
       <jm-input v-model="input3" readonly />
     </white-space>
     <white-space>
-      <div slot="desc">清空按钮</div>
+      <div slot="desc">清空按钮 <code>clearable</code></div>
       <jm-input v-model="input4" clearable />
+    </white-space>
+    <white-space>
+      <div slot="desc">密码框 <code>show-password</code></div>
+      <jm-input v-model="input5" clearable show-password />
+    </white-space>
+    <white-space>
+      <div slot="desc">设置前后Icon <code>prefix-icon</code> 、 <code>suffix-icon</code> 或者设置 <code>prefix/suffix</code> 的slot</div>
+      <jm-input v-model="input6" prefix-icon="jm-icon-person" suffix-icon="jm-icon-tickets" />
+    </white-space>
+    <white-space>
+      <div slot="desc">字数限制 <code>show-word-limit</code></div>
+      <jm-input v-model="input7" maxlength="20" show-word-limit />
+    </white-space>
+    <white-space :hor="0">
+      <div slot="desc">textarea</div>
+      <jm-input type="textarea" maxlength="10" ref="textarea" v-model="input8" />
     </white-space>
   </div>
 </template>
@@ -26,7 +42,11 @@ export default {
       input1: '',
       input2: '这是禁用状态',
       input3: '这是只读状态',
-      input4: '支持清空'
+      input4: '支持清空',
+      input5: '1234',
+      input6: '',
+      input7: '',
+      input8: '这是文本域'
     }
   }
 }
