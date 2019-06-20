3b3cbdb2c95769be5b0bba368c433a3b151f1cb9
zhongzhijie <zhongzhijie@jd.com>
2019-06-20 21:56:30 +0800

    button 按钮

--- examples/layout/header.vue
+++ examples/layout/header.vue
@@ -2,7 +2,8 @@
   <header class="header">
     <div class="header-container">
       <router-link :to="{ path: '/' }" class="logo-block">
-        JM Design
+        <i class="jm-design-logo"></i>
+        <span>JM Design</span>
       </router-link>
       <ul class="header-tab">
         <li class="header-tab__item" v-for="(page, key) in pages" :key="key">
@@ -32,5 +33,78 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../assets/style/layout/header.scss';
+/**
+ * 布局文件 header 的样式
+ */
+@import '../assets/style/variable';
+
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border-bottom: 1px solid #eee;
+  background: $color-bg;
+  z-index: 100;
+}
+.header-container {
+  margin: 0 120px;
+  height: $layout-header-height;
+  line-height: $layout-header-height;
+}
+.logo-block {
+  display: inline-block;
+  height: $layout-header-height;
+  line-height: $layout-header-height;
+  vertical-align: top;
+  font-size: 18px;
+  color: $color-theme;
+
+  span {
+    vertical-align: middle;
+  }
+}
+.jm-design-logo {
+  display: inline-block;
+  margin-right: 10px;
+  width: 40px;
+  height: 40px;
+  background: url('../assets/img/jm-design.png') no-repeat;
+  background-size: cover;
+  vertical-align: middle;
+}
+.header-tab {
+  float: right;
+}
+.header-tab__item {
+  display: inline-block;
+}
+.header-tab__link {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: $fs-title;
+  line-height: 25px;
+  color: $color-text-light;
+
+  &:hover {
+    color: $color-important;
+  }
+}
+.header-tab__link--active{
+  color: $color-theme;
+
+  &:hover {
+    color: $color-theme;
+  }
+  &::after {
+    position: absolute;
+    content: '';
+    width: 70%;
+    height: 2px;
+    left: 15%;
+    bottom: 0;
+    background: $color-theme;
+  }
+}
 </style>
--- examples/main.js
+++ examples/main.js
@@ -3,11 +3,8 @@ import router from './route'
 import App from './App'
 import './assets/style/base.scss'
 import './assets/fonts/iconfont.css'
-import DemoBlock from './components/demoBlock.vue'
 
 Vue.config.productionTip = false
-
-Vue.component(DemoBlock.name, DemoBlock)
 
 /* eslint-disable no-new */
 new Vue({
