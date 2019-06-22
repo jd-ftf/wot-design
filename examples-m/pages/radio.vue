2c68e4fe85aa69ffb170d31e073ea52c7a5e4db3
zhongzhijie <zhongzhijie@jd.com>
2019-06-22 14:28:07 +0800

    radio

---
+++ examples-m/pages/radio.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <white-space>
+      <div slot="desc">基础用法</div>
+      <jm-radio-group v-model="radio1">
+        <jm-radio value="1">单选框1</jm-radio>
+        <jm-radio value="2">单选框2</jm-radio>
+      </jm-radio-group>
+    </white-space>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      radio1: '1'
+    }
+  }
+}
+</script>
---
+++ examples/docs/radio.md
@@ -0,0 +1,14 @@
+## Radio 单选框
+
+### 使用
+
+#### 按需引入
+
+```javascript
+import Vue from 'vue'
+import { Radio } from 'jm-design'
+
+Vue.use(Radio)
+```
+
+#### 示例
