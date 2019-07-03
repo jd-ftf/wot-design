853f5f0dd0a2893ea9c4855261ce94e3db616e46
zhongzhijie <zhongzhijie@jd.com>
2019-07-03 14:56:51 +0800

    按钮和输入框样式修改

--- packages/inputNumber/src/main.vue
+++ packages/inputNumber/src/main.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="jm-input-number">
-
+    <a class="jm-input-number__action">
+      <i class="jm-input-number__sub"></i>
+    </a>
+    <a class="jm-input-number__action">
+      <i class="jm-input-number__add"></i>
+    </a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'JmInputNumber'
+  name: 'JmInputNumber',
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    min: {
+      type: Number,
+      default: -Infinity
+    },
+    max: {
+      type: Number,
+      default: Infinity
+    },
+    step: {
+      type: Number,
+      default: 1
+    },
+    stepStrictly: Boolean,
+    precision: {
+      type: Number,
+      default: 0
+    },
+    disabled: Boolean,
+    withoutInput: Boolean,
+    inputWidth: String
+  }
 }
 </script>
