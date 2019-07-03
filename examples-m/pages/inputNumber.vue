84461fed383e15f04f689292b6a82a47f171985b
zhongzhijie <zhongzhijie@jd.com>
2019-07-03 21:51:43 +0800

    计数器

--- examples-m/pages/inputNumber.vue
+++ examples-m/pages/inputNumber.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>
+  <div class="page-input-number">
     <white-space>
       <template #desc>
         <div>基本用法</div>
       </template>
       <jm-input-number v-model="value1" />
+    </white-space>
+    <white-space>
+      <template #desc>
+        <div>设置步长<code>step</code></div>
+      </template>
+      <jm-input-number v-model="value2" :step="2" />
+    </white-space>
+    <white-space>
+      <template #desc>
+        <div>限制最小最大值<code>min</code>、<code>max</code></div>
+      </template>
+      <jm-input-number v-model="value3" :min="3" :max="10" />
+    </white-space>
+    <white-space>
+      <template #desc>
+        <div>禁用<code>disabled</code></div>
+      </template>
+      <jm-input-number v-model="value4" disabled />
+    </white-space>
+    <white-space>
+      <template #desc>
+        <div>无输入框<code>without-input</code></div>
+      </template>
+      <div class="flex">
+        <div>数量：{{ value5 }}</div>
+        <jm-input-number class="input-number-inline" v-model="value5" without-input />
+      </div>
+    </white-space>
+    <white-space>
+      <template #desc>
+        <div>设置精度<code>precision</code></div>
+      </template>
+      <jm-input-number v-model="value6" :precision="2" :step="0.1" />
     </white-space>
   </div>
 </template>
@@ -13,8 +46,23 @@
 export default {
   data () {
     return {
-      value1: 0
+      value1: 1,
+      value2: 1,
+      value3: 4,
+      value4: 2,
+      value5: 1,
+      value6: 1
     }
   }
 }
 </script>
+
+<style lang="scss">
+.page-input-number {
+  .flex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
--- packages/inputNumber/src/main.vue
+++ packages/inputNumber/src/main.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="jm-input-number">
-    <a class="jm-input-number__action">
+  <div
+    class="jm-input-number"
+    :class="{
+      'is-disabled': disabled,
+      'is-without-input': withoutInput
+    }"
+  >
+    <a class="jm-input-number__action" :class="{ 'is-disabled': minDisabled }" @click="sub">
       <i class="jm-input-number__sub"></i>
     </a>
-    <a class="jm-input-number__action">
+    <div v-if="!withoutInput" class="jm-input-number__inner">
+      <input
+        class="jm-input-number__input"
+        type="number"
+        :min="min"
+        :max="max"
+        :disabled="disabled"
+        :value="currentValue"
+        @input="handleInput"
+        @blur="handleBlur"
+      />
+      <i class="jm-input-number__input-border"></i>
+    </div>
+    <a class="jm-input-number__action" :class="{ 'is-disabled': maxDisabled }" @click="add">
       <i class="jm-input-number__add"></i>
     </a>
   </div>
@@ -12,14 +31,16 @@
 <script>
 export default {
   name: 'JmInputNumber',
+  data () {
+    return {
+      currentValue: ''
+    }
+  },
   props: {
-    value: {
-      type: Number,
-      default: 0
-    },
+    value: [Number, String],
     min: {
       type: Number,
-      default: -Infinity
+      default: 1
     },
     max: {
       type: Number,
@@ -37,6 +58,47 @@ export default {
     disabled: Boolean,
     withoutInput: Boolean,
     inputWidth: String
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler (value) {
+        this.currentValue = value
+      }
+    }
+  },
+  computed: {
+    minDisabled () {
+      return this.disabled || this.value <= this.min || (this.value - this.step) < this.min
+    },
+    maxDisabled () {
+      return this.disabled || this.value >= this.max || (this.value + this.step) > this.max
+    }
+  },
+  methods: {
+    sub () {
+      if (this.minDisabled) return
+
+      this.$emit('input', parseFloat(this.value) - this.step)
+    },
+    add () {
+      if (this.maxDisabled) return
+
+      this.$emit('input', parseFloat(this.value) + this.step)
+    },
+    handleInput (event) {
+      let value = parseFloat(event.target.value)
+      this.$emit('input', value)
+    },
+    handleBlur () {
+      if (!this.value) {
+        this.$emit('input', this.min)
+      }
+    },
+    format (value) {
+      value = value || 0
+      return parseFloat(value).toFixed(this.precision)
+    }
   }
 }
 </script>
