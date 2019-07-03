b23ac2a3622e7024a9e635e0e6eaca552eb56c9c
zhongzhijie <zhongzhijie@jd.com>
2019-07-03 14:56:51 +0800

    按钮和输入框样式修改

--- examples-m/pages/inputNumber.vue
+++ examples-m/pages/inputNumber.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-
+    <white-space>
+      <template #desc>
+        <div>基本用法</div>
+      </template>
+      <jm-input-number v-model="value1" />
+    </white-space>
   </div>
 </template>
 
 <script>
 export default {
   data () {
+    return {
+      value1: 0
+    }
   }
 }
 </script>
