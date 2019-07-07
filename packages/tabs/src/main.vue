bcea5c0f2bbc91a407b802e9d1da5cabce5b022d
zhongzhijie <zhongzhijie@jd.com>
2019-07-07 16:19:24 +0800

    tabs 配置修改

--- packages/tabs/src/main.vue
+++ packages/tabs/src/main.vue
@@ -6,6 +6,38 @@
 
 <script>
 export default {
-  name: 'JmTabs'
+  name: 'JmTabs',
+  data () {
+    return {
+      items: []
+    }
+  },
+  props: {
+    value: {
+      type: [Number, String],
+      default: 0
+    },
+    slidableNum: {
+      type: Number,
+      default: 6
+    },
+    color: String,
+    inactiveColor: String,
+    sticky: Boolean,
+    animated: Boolean,
+    swipeable: Boolean,
+    lazyRender: Boolean,
+    lineWidth: Number,
+    lineHeight: {
+      type: String,
+      default: '3px'
+    },
+    disableMap: Boolean
+  },
+  computed: {
+    tabLength () {
+      return this.items.length
+    }
+  }
 }
 </script>
