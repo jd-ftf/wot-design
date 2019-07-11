fed2a75508cc19d12e3eda8ed72653f81da2317f
zhongzhijie <zhongzhijie@jd.com>
2019-07-11 09:03:02 +0800

    tab 部分功能

--- packages/tab/src/main.vue
+++ packages/tab/src/main.vue
@@ -1,22 +1,45 @@
 <template>
-  <div class="jm-tab">
-    <slot></slot>
+  <div class="jm-tab" :style="tabStyle">
+    <div class="jm-tab__body">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'JmTab',
+  inject: ['tabs'],
   props: {
     title: String,
     name: String,
     disabled: Boolean
   },
-  beforeCreate () {
-    this.$parent.items.push(this)
+  computed: {
+    index () {
+      return this.tabs.items.indexOf(this)
+    },
+    isActive () {
+      return this.index === this.tabs.activeIndex
+    },
+    tabStyle () {
+      let sty
+      if (!this.isActive) {
+        sty = {
+          height: 0,
+          overflow: 'visible'
+        }
+      }
+      return sty
+    }
+  },
+  created () {
+    if (this.tabs) {
+      this.tabs.items.push(this)
+    }
   },
   destroyed () {
-    this.$parent.items.slice(this.$parent.items.indexOf(this), 1)
+    this.tabs.items.slice(this.tabs.items.indexOf(this), 1)
   }
 }
 </script>
