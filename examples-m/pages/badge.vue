93c880906ba0495c1ba74545755739f141c34439
zhongzhijie <zhongzhijie@jd.com>
2019-07-11 09:21:59 +0800

    角标细节调整

--- examples-m/pages/badge.vue
+++ examples-m/pages/badge.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="page-button">
+  <div class="page-badge">
     <white-space>
       <div slot="desc">展示消息数量<code>value</code></div>
-      <jm-badge :value="12" class="item ">
+      <jm-badge :value="12" class="item">
         <jm-button size="small">评论</jm-button>
       </jm-badge>
-      <jm-badge :value="3" class="item " bgColor="pink">
+      <jm-badge :value="3" class="item" bg-color="pink">
         <jm-button size="small">回复</jm-button>
       </jm-badge>
-      <jm-badge :value="1" class="item " type="primary">
+      <jm-badge :value="1" class="item" type="primary">
         <jm-button size="small">评论</jm-button>
       </jm-badge>
       <jm-badge :value="2" class="item" type="warning">
         <jm-button size="small">回复</jm-button>
       </jm-badge>
+      <jm-badge :value="1" class="item" type="success">
+        <jm-button size="small">评论</jm-button>
+      </jm-badge>
+      <jm-badge :value="2" class="item" type="info">
+        <jm-button size="small">回复</jm-button>
+      </jm-badge>
     </white-space>
     <white-space>
       <div slot="desc">可定义消息最大值<code>max</code></div>
-      <jm-badge :value="200" :max="99" class="item ">
+      <jm-badge :value="200" :max="99" class="item">
         <jm-button size="small">评论</jm-button>
       </jm-badge>
       <jm-badge :value="100" :max="10" class="item ">
@@ -25,8 +31,8 @@
       </jm-badge>
     </white-space>
     <white-space>
-      <div slot="desc">可自己定制内容<code>value</code></div>
-      <jm-badge value="new" class="item " bgColor="red">
+      <div slot="desc">可自己定制内容<code>value</code>，value 设置为字符串</div>
+      <jm-badge value="new" class="item">
         <jm-button size="small">评论</jm-button>
       </jm-badge>
       <jm-badge value="hot" class="item">
@@ -49,8 +55,10 @@ export default {
 }
 </script>
 <style lang="scss">
-.item{
-  margin-top:10px;
-  margin-right:25px;
+.page-badge {
+  .item{
+    margin-top:10px;
+    margin-right:25px;
+  }
 }
 </style>
