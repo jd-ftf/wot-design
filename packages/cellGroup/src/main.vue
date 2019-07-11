d240a6b69bbd31ca042537449deda776c25d9cea
zhongzhijie <zhongzhijie@jd.com>
2019-07-11 10:20:57 +0800

    cell 调整，代码和样式都不符合规范

---
+++ packages/cellGroup/src/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jm-cell-group">
+    <slot name="title">
+      <div v-if="title" class="jm-cell-group__title">{{ title }}</div>
+    </slot>
+    <div class="jm-cell-group__body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JmCellGroup',
+  props: {
+    title: String
+  }
+}
+</script>
