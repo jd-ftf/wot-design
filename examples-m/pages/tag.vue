a3a734065382c8f2071932586b7b94d05bce44e6
zhongzhijie <zhongzhijie@jd.com>
2019-08-15 10:16:43 +0800

    Tag 标签

--- examples-m/pages/tag.vue
+++ examples-m/pages/tag.vue
@@ -15,17 +15,46 @@
       <jm-tag type="success" plain>标签</jm-tag>
     </white-space>
     <white-space title="标签大小">
+      <jm-tag size="small">标签</jm-tag>
       <jm-tag>标签</jm-tag>
       <jm-tag size="large">标签</jm-tag>
     </white-space>
     <white-space title="自定义颜色">
       <jm-tag color="#0083ff" bg-color="#d0e8ff">标签</jm-tag>
+      <jm-tag color="#faa21e" bg-color="#faa21e" plain>标签</jm-tag>
     </white-space>
     <white-space title="设置图标">
       <jm-tag icon="jm-icon-tickets">标签</jm-tag>
     </white-space>
+    <white-space title="可关闭">
+      <jm-tag v-for="tag in tags" :key="tag.name" type="primary" closable :size="tag.size" plain @close="handleClose(tag)">{{ tag.name }}</jm-tag>
+    </white-space>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tags: [{
+        name: '标签一',
+        size: 'small'
+      }, {
+        name: '标签二',
+        size: 'medium'
+      }, {
+        name: '标签三',
+        size: 'large'
+      }]
+    }
+  },
+  methods: {
+    handleClose (tag) {
+      this.tags.splice(this.tags.indexOf(tag), 1)
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 .page-tag {
