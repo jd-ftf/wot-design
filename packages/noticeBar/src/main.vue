1f2611f500fc0efeaabaa2e1164a53e4c89012eb
zhongzhijie <zhongzhijie@jd.com>
2019-10-05 16:43:42 +0800

    NoticeBar finished

--- packages/noticeBar/src/main.vue
+++ packages/noticeBar/src/main.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="jm-notice-bar" :style="{ background: backgroundColor, color: color }">
+  <div
+    v-if="show"
+    class="jm-notice-bar"
+    :class="{
+      'jm-notice-bar--ellipse': !wrapable && !scrollable,
+      'jm-notice-bar--wrap': wrapable && !scrollable
+    }"
+    :style="{ 'background': backgroundColor, 'color': color }"
+  >
     <slot name="left-icon">
-      <i :class="`jm-notice-bar__left-icon jm-icon-${leftIcon}`"></i>
+      <i v-if="leftIcon" :class="`jm-notice-bar__left-icon jm-icon-${leftIcon}`"></i>
     </slot>
     <div class="jm-notice-bar__wrap" ref="wrap">
-      <div class="jm-notice-bar__content" ref="content">
+      <div
+        class="jm-notice-bar__content"
+        :class="[ animationClass ]"
+        :style="{
+          'padding-left': (firstPlay ? 0 : wrapWidth) + 'px',
+          'animation-delay': (firstPlay ? delay : 0) + 's',
+          'animation-duration': duration + 's'
+        }"
+        ref="content"
+        @animationend="animationEnd"
+        @webkitAnimationEnd="animationEnd"
+      >
         <slot>
           {{ text }}
         </slot>
@@ -19,6 +38,16 @@
 <script>
 export default {
   name: 'JmNoticeBar',
+  data () {
+    return {
+      firstPlay: true,
+      animationClass: '',
+      wrapWidth: 0,
+      contentWidth: 0,
+      show: true,
+      duration: 0
+    }
+  },
   props: {
     text: String,
     scrollable: {
@@ -39,9 +68,37 @@ export default {
     color: String,
     backgroundColor: String
   },
+  watch: {
+    text: {
+      immediate: true,
+      handler () {
+        this.$nextTick(() => {
+          if (!this.$refs.wrap || !this.$refs.content) return
+
+          const wrapWidth = this.$refs.wrap.getBoundingClientRect().width
+          const contentWidth = this.$refs.content.getBoundingClientRect().width
+
+          if (this.scrollable && contentWidth > wrapWidth) {
+            this.wrapWidth = wrapWidth
+            this.contentWidth = contentWidth
+            this.duration = contentWidth / this.speed
+            this.animationClass = 'jm-notice-bar__content--play'
+          }
+        })
+      }
+    }
+  },
   methods: {
-    handleClick () {
-      this.$emit('click')
+    handleClick (event) {
+      this.show = false
+      this.$emit('click', event)
+    },
+    animationEnd () {
+      this.firstPlay = false
+      this.$nextTick(() => {
+        this.duration = (this.wrapWidth + this.contentWidth) / this.speed
+        this.animationClass = 'jm-notice-bar__content--play-infinite'
+      })
     }
   }
 }
