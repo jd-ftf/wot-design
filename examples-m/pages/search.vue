249d81f09bdb2a0fe7a17cc67965bf67c67a9363
zhongzhijie <zhongzhijie@jd.com>
2019-10-27 21:21:13 +0800

    文档修改

--- examples-m/pages/search.vue
+++ examples-m/pages/search.vue
@@ -1,35 +1,20 @@
 <template>
   <div>
-    <white-space :hor="0">
-      <div slot="desc">搜索，监听对应事件<code>search、cancel、clear</code>事件</div>
-      <jm-search
-        v-model="search1"
-        @search="handleSearch"
-        @cancel="handleCancel"
-        @clear="handleClear"
-      />
-    </white-space>
-    <white-space :hor="0">
-      <div slot="desc"><code>light</code>为<code>true</code>的搜索，监听<code>focus</code>和<code>blur</code>事件</div>
-      <jm-search
-        v-model="search2"
-        light
-        @focus="handleFocus1"
-        @blur="handleBlur"
-      />
-    </white-space>
-    <white-space :hor="0">
-      <div slot="desc"><code>placeholder-left</code>为<code>true</code>的搜索</div>
+    <space title="基本用法">
+      <jm-search v-model="search1" @search="handleSearch" @cancel="handleCancel" @clear="handleClear" />
+    </space>
+    <space title="白色输入框">
+      <jm-search v-model="search2" light />
+    </space>
+    <space title="搜索占位符居左">
       <jm-search v-model="search3" placeholder-left />
-    </white-space>
-    <white-space :hor="0">
-      <div slot="desc"><code>hide-cancel</code>为<code>true</code>，且<code>disabled</code>为<code>true</code>的搜索</div>
-      <jm-search v-model="search4" @focus="handleFocus2" hide-cancel disabled />
-    </white-space>
-    <white-space :hor="0">
-      <div slot="desc">修改<code>placeholder</code>和<code>cancel-txt</code></div>
+    </space>
+    <space title="禁用且隐藏取消按钮">
+      <jm-search v-model="search4" hide-cancel disabled />
+    </space>
+    <space title="自定义">
       <jm-search v-model="search5" placeholder="请输入订单号/订单名称" cancel-txt="搜索" />
-    </white-space>
+    </space>
   </div>
 </template>
 
@@ -49,20 +34,11 @@ export default {
     handleSearch (val) {
       this.$toast(`搜索：${val}`)
     },
-    handleFocus1 () {
-      this.$toast('聚焦搜索框')
-    },
-    handleBlur () {
-      this.$toast('搜索框失去焦点')
-    },
     handleCancel () {
       this.$toast('点击了取消按钮')
     },
     handleClear () {
       this.$toast('点击了清除按钮')
-    },
-    handleFocus2 () {
-      this.$toast('点击了搜索框，但不会聚焦，因为搜索框不可用')
     }
   }
 }
