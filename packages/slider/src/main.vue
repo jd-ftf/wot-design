7716d1fc836208210539b60553ad9c7b4095811d
houxinchao <houxinchao@jd.com>
2019-12-20 11:15:41 +0800

    fix(slider): 逻辑修改

--- packages/slider/src/main.vue
+++ packages/slider/src/main.vue
@@ -1,69 +1,62 @@
 <template>
   <div
+    ref="track"
     :class="{
       'jm-slider': true,
-      'jm-slider--has-label': !hideLabel,
+      'jm-slider__has-label': !hideLabel,
       'jm-slider--disabled': disabled
-    }">
+    }"
+  >
     <div
       class="jm-slider__label-min"
       v-if="!hideMinMax">
       {{ minValue }}
     </div>
     <div
-      ref="axle"
-      :class="{
-        'jm-slider__axle': true,
-        'jm-slider__axle--has-min-max': !hideMinMax
-      }"
-      @touchstart="slidingStart"
-      @touchend="slidingEnd"
-      @touchmove="sliding">
+      class="jm-slider__bar-wrapper"
+      :style="{background: inactiveColor ? inactiveColor : '' } "
+    >
       <div
-        ref="container"
-        v-if="!disabled"
-        class="jm-slider__handle-container"
-        :style="{ left: handlePosition[0] - handleRadius + 'px' }">
-        <div class="jm-slider__handle"></div>
-        <div
-          class="jm-slider__label-cur"
-          v-if="!hideLabel">
-          {{ typeof value === 'number' ? value : value[0] }}
-        </div>
+        class="jm-slider__bar"
+        :style="{ background: activeColor ? activeColor : '',
+                  width: barWidth + '%',
+                  left: barLeft + '%'
+                }"
+      ></div>
+      <!-- 左边 -->
+      <div
+        class="jm-slider__button-wrapper"
+        :style="{left:  leftBarPercent - 10 + '%', visibility: !disabled ? 'show' : 'hidden' }"
+        @touchstart="onTouchStart"
+        @touchmove="onTouchMove"
+        @touchend="onTouchEnd"
+      >
+        <div class="jm-slider__label" v-if="!hideLabel">{{ leftNewValue }}</div>
+        <div class="jm-slider__button" />
       </div>
+      <!-- 右边 -->
       <div
-        v-if="!disabled && typeof value !== 'number'"
-        :style="{ left: handlePosition[1] - handleRadius + 'px' }"
-        class="jm-slider__handle-container">
-        <div class="jm-slider__handle"></div>
-        <div
-          class="jm-slider__label-cur"
-          v-if="!hideLabel">
-          {{ value[1] }}
-        </div>
-      </div>
-      <div
-        class="jm-slider__progress-bar"
-        :style="{
-          width: Math.abs(handlePosition[0] - handlePosition[1]) + 'px',
-          left: Math.min(handlePosition[0], handlePosition[1]) + 'px'
-        }">
+        v-if="showRight"
+        class="jm-slider__button-wrapper"
+        :style="{ left: rightBarPercent - 10 + '%' , visibility: !disabled ? 'show' : 'hidden' } "
+        @touchstart="onTouchStartRight"
+        @touchmove="onTouchMoveRight"
+        @touchend="onTouchEndRight"
+      >
+        <div class="jm-slider__label" v-if="!hideLabel">{{ rightNewValue }}</div>
+        <div class="jm-slider__button" />
       </div>
     </div>
-    <div
-      class="jm-slider__label-max"
-      v-if="!hideMinMax">
-      {{ maxValue }}
-    </div>
+    <div class="jm-slider__label-max" v-if="!hideMinMax">{{ maxValue }}</div>
   </div>
 </template>
 
 <script>
-/**
- * Todo: 双滑块有bug，而且api的约定不太好，待优化
- */
+import touchMixin from '@/mixins/touch'
+
 export default {
   name: 'JmSlider',
+  mixins: [touchMixin],
   props: {
     hideMinMax: Boolean,
     hideLabel: Boolean,
@@ -86,90 +79,171 @@ export default {
     step: {
       type: Number,
       default: 1
+    },
+    inactiveColor: {
+      type: String,
+      default: '#e5e5e5'
+    },
+    activeColor: {
+      type: String,
+      default: ''
     }
   },
   data () {
     return {
-      handleRadius: 0,
-      axleWidth: 0
+      showRight: false,
+      barWidth: '',
+      barLeft: '',
+      barHeight: '6px',
+      leftNewValue: 0,
+      rightNewValue: 0,
+      rightBarPercent: 0,
+      leftBarPercent: 0
     }
   },
-  computed: {
-    handlePosition () {
-      return typeof this.value === 'number'
-        ? [this.value2Pos(this.value), 0]
-        : [this.value2Pos(this.value[0]), this.value2Pos(this.value[1])]
-    }
+  created () {
+    this.updateValue()
   },
   mounted () {
-    this.handleRadius = this.$refs.container && this.$refs.container.clientWidth / 2
-    this.axleWidth = this.$refs.axle.clientWidth
+    // 获取轨道长度
+    this.trackWidth = this.$refs.track && this.$refs.track.clientWidth
     window.onresize = () => {
-      this.axleWidth = this.$refs.axle.clientWidth
+      this.trackWidth = this.$refs.track && this.$refs.track.clientWidth
     }
   },
   methods: {
-
-    // 开始拖动事件
-    slidingStart () {
-      if (!this.disabled) {
-        this.$emit('slidingstart', this.value)
-      }
-    },
-
-    // 拖动事件
-    sliding (event) {
-      if (!this.disabled) {
-        const touchX = event.changedTouches[0].clientX
-        const axleX = this.$refs.axle.offsetLeft
-        const currentPos = touchX - axleX
-
-        if (typeof this.value === 'number') {
-          const value = this.pos2Value(currentPos)
-          this.$emit('sliding', value)
-          this.$emit('input', value)
+    updateValue () {
+      const { value } = this
+      // 动态传值后修改
+      if (this.checkType(value) === 'Array') {
+        this.showRight = true
+        const { leftBarPercent, rightBarPercent } = this
+        if (leftBarPercent < rightBarPercent) {
+          this.leftBarSlider(value[0])
+          this.rightBarSlider(value[1])
         } else {
-          const deltaLeft = Math.abs(currentPos - this.value2Pos(this.value[0]))
-          const deltaRight = Math.abs(currentPos - this.value2Pos(this.value[1]))
-
-          const value = this.pos2Value(currentPos)
-          const currentValue = deltaLeft < deltaRight
-            ? [value, this.value[1]]
-            : [this.value[0], value]
-          this.$emit('sliding', currentValue)
-          this.$emit('input', currentValue)
+          this.leftBarSlider(value[1])
+          this.rightBarSlider(value[0])
         }
+      } else {
+        this.leftBarSlider(value)
       }
     },
-
-    // 结束拖动事件
-    slidingEnd () {
-      if (!this.disabled) {
-        this.$emit('slidingend', this.value)
+    onTouchStart (even) {
+      if (this.disabled) return
+      const { leftBarPercent, value, rightBarPercent } = this
+      this.touchStart(event)
+      // 是左边滑轮
+      this.startValue = this.checkType(value) !== 'Array' ? this.format(value)
+        : (leftBarPercent < rightBarPercent ? this.format(value[0]) : this.format(value[1]))
+    },
+    onTouchMove (event) {
+      if (this.disabled) return
+      const { maxValue, minValue } = this
+      this.touchMove(event)
+      // 移动间距 this.deltaX 就是向左(-)向右(+)
+      const diff = this.deltaX / this.trackWidth * (maxValue - minValue)
+      this.newValue = this.startValue + diff
+      // 左滑轮滑动控制
+      this.leftBarSlider(this.newValue)
+      this.$emit('dragmove', this.currentValue)
+    },
+    onTouchEnd () {
+      /* eslint-disable no-useless-return */
+      if (this.disabled) return
+      this.$emit('dragend', this.currentValue)
+    },
+    // 右边滑轮滑动状态监听
+    onTouchStartRight (event) {
+      if (this.disabled) return
+      const { leftBarPercent, rightBarPercent, value } = this
+      // 右滑轮移动时数据绑定
+      this.touchStart(event)
+      // 记录开始数据值
+      this.startValue = leftBarPercent < rightBarPercent ? this.format(value[1]) : this.format(value[0])
+      this.$emit('dragstart', this.currentValue)
+    },
+    onTouchMoveRight (event) {
+      if (this.disabled) return
+      const { maxValue, minValue } = this
+      this.touchMove(event)
+      // 移动间距 this.deltaX 就是向左向右
+      const diff = this.deltaX / this.trackWidth * (maxValue - minValue)
+      this.newValue = this.format(this.startValue + diff)
+      // 右滑轮滑动控制
+      this.rightBarSlider(this.newValue)
+      this.$emit('dragmove', this.currentValue)
+    },
+    onTouchEndRight () {
+      if (this.disabled) return
+      this.$emit('dragend', this.currentValue)
+    },
+    /**
+     * 控制右侧滑轮滑动， value校验
+     * @param {Number} value 当前滑轮绑定值
+     */
+    rightBarSlider (value) {
+      const { minValue, maxValue } = this
+      const rightBarPercent = (value - minValue) / (maxValue - minValue) * 100
+      this.rightNewValue = this.format(value)
+      this.rightBarPercent = this.format(rightBarPercent)
+      this.styleControl()
+    },
+    /**
+     * 控制左滑轮滑动，更新渲染数据，对 value 进行校验取整
+     * @param {Number} value 当前滑轮绑定值
+     */
+    leftBarSlider (value) {
+      const { minValue, maxValue, showRight } = this
+      value = this.format(value)
+      // 把 value 转换成百分比
+      const percent = this.format((value - minValue) / (maxValue - minValue) * 100)
+      console.log(percent)
+      this.leftNewValue = value
+      this.leftBarPercent = this.format(percent)
+      if (!showRight) {
+        this.barWidth = percent
+      } else {
+        this.styleControl()
       }
     },
-
-    // 如果value超出限定值则设定为限定值
-    fixValue (value) {
-      value < this.minValue && (value = this.minValue)
-      value > this.maxValue && (value = this.maxValue)
-      return value
+    // 样式控制
+    styleControl () {
+      const { leftNewValue, rightNewValue } = this
+      if (!leftNewValue || !rightNewValue) return
+      const { leftBarPercent, rightBarPercent } = this
+      // 左右滑轮距离左边最短为当前激活条所处位置
+      const barLeft = leftBarPercent < rightBarPercent
+        ? [leftBarPercent, rightBarPercent]
+        : [rightBarPercent, leftBarPercent]
+      this.barWidth = barLeft[1] - barLeft[0]
+      this.barLeft = barLeft[0]
     },
-
     // 将pos转化为value
     pos2Value (pos) {
-      const percent = pos / this.axleWidth
-      const value = percent * (this.maxValue - this.minValue) + this.minValue
-      const res = this.minValue + Math.floor((value - this.minValue) / this.step) * this.step
-      return this.fixValue(res)
+      const { maxValue, minValue, step } = this
+      const percent = pos / this.trackWidth
+      const value = percent * (maxValue - minValue) + minValue
+      const res = minValue + Math.floor((value - minValue) / step) * step
+      return res
     },
-
-    // 将value转化为pos
-    value2Pos (value) {
-      const fixedValue = this.fixValue(value)
-      const percent = (fixedValue - this.minValue) / (this.maxValue - this.minValue)
-      return percent * this.axleWidth
+    checkType (value) {
+      return Object.prototype.toString.call(value).slice(8, -1)
+    },
+    equal (arr1, arr2) {
+      let i = 0
+      arr1.forEach((item, index) => {
+        item === arr2[index] && i++
+      })
+      return (i === 2)
+    },
+    format (value) {
+      const { maxValue, minValue, step } = this
+      return Math.round(Math.max(minValue, Math.min(value, maxValue)) / step) * step
     }
   }
 }
 </script>
+
+<style>
+</style>
