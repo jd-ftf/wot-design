d4f123525e9b94b7d1498287cb1f6918ab382e7f
zhongzhijie <zhongzhijie@jd.com>
2019-12-23 10:07:07 +0800

    config: add commitlint config and modify the config of eslint

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'packages/**/*.js': [
+    'cross-env EFF_ABSOLUTE_PATHS=true eslint --fix --format friendly',
+    'git add'
+  ],
+  'src/**/*.js': [
+    'cross-env EFF_ABSOLUTE_PATHS=true eslint --fix --format friendly',
+    'git add'
+  ]
+}
