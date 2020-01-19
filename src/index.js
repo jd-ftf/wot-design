e7c8fa7114de26c994b550da81809c62ca0df5b0
houxinchao <houxinchao@jd.com>
2020-01-19 17:37:50 +0800

    feat: 新增tooltip组件
    docs: 添加文档搜索
    
    nothing: 调整内容，删除多余样式
    
    fix: 搜索功能添加、tooltip删除部分多余代码
    
    fix: tooltip组件生效
    
    fix: 列表模式添加
    
    fix: 主题更换功能添加
    
    docs: 文档修改
    
    fix: 注释修改
    
    fix: 逻辑小修
    
    fix: 添加点击外部关闭功能，添加手动控制显隐，移除文档动画操作，添加菜单事件
    
    fix: 移除打印、文档延时属性修改
    
    style: 示例样式修改
    
    fix: 补齐代码
    
    fix: 延时显示添加、显隐属性双向绑定、方法名替换，文档校准
    
    fix: 公用逻辑抽离，外部控制功能修复，修改方法名，文档添加注意部分
    
    docs: 文档与demo对齐，删除open-delay属性

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ import Tabbar from '../packages/tabbar'
 import TabbarItem from '../packages/tabbar-item'
 import Tag from '../packages/tag'
 import Toast from '../packages/toast'
+import Tooltip from '../packages/tooltip'
 import locale from './locale'
 
 const components = [
@@ -87,6 +88,7 @@ const components = [
   Tabbar,
   TabbarItem,
   Tag,
+  Tooltip,
   MessageBox.wdMessageBox
 ]
 
@@ -155,5 +157,6 @@ export default {
   Tabbar,
   TabbarItem,
   Tag,
-  Toast
+  Toast,
+  Tooltip
 }
