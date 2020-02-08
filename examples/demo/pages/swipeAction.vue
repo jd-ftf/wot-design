5e9cacce3bf4992b327da5601c954b1cad81273b
Gkxie <xieyu141419@jd.com>
2020-02-08 22:18:57 +0800

    feat(swipeAction): swipeAction package complete

---
+++ examples/demo/pages/swipeAction.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <space title="基本用法">
+      <wd-swipe-action v-model="value" :before-close="beforeClose">
+        <div slot="left" class="action">
+          <div class="button" style="background: #C8C7CD;">操作1</div>
+          <div class="button" style="background: #FFB300;">操作2</div>
+          <div class="button" style="background: #E2231A;">操作3</div>
+        </div>
+        <wd-cell title="标题文字" value="内容"/>
+        <div slot="right" class="action">
+          <div class="button" style="background: #C8C7CD;">操作1</div>
+          <div class="button" style="background: #FFB300;">操作2</div>
+          <div class="button" style="background: #E2231A;">操作3</div>
+        </div>
+      </wd-swipe-action>
+    </space>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: 'right'
+    }
+  },
+  methods: {
+    beforeClose (reason, position) {
+    }
+  }
+}
+</script>
+<style>
+  .action {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+  .button{
+    display: inline-block;
+    width: 40px;
+    height: 100%;
+    color: white;
+    line-height: 46px;
+    text-align: center;
+  }
+</style>
