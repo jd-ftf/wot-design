fc2e5007b547db566e1ba8ed3713d67dbdb0d05f
houtianfu <houtianfu@jd.com>
2020-02-26 18:31:54 +0800

    img: init

---
+++ examples/demo/pages/img.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-img">
+    <demo-block title="基本用法">
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+    </demo-block>
+    <demo-block title="填充">
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+    </demo-block>
+    <demo-block title="圆形">
+      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+    </demo-block>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
