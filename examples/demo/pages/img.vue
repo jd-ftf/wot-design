813a2ec9dfe1ea907c1ecd81911f4a38a6d3d32b
houtianfu <houtianfu@jd.com>
2020-02-28 18:38:30 +0800

    img: 完成图片增强组件的基本功能

--- examples/demo/pages/img.vue
+++ examples/demo/pages/img.vue
@@ -1,29 +1,46 @@
 <template>
   <div class="page-img">
     <demo-block title="基本用法">
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <wd-img width="100" height="100" :src="joy" alt="joy" />
     </demo-block>
     <demo-block title="填充">
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <div class="col" v-for="fit in fits">
+        <wd-img width="100%" height="27vw" :src="joy" :fit="fit" />
+        <div class="center">{{fit}}</div>
+      </div>
     </demo-block>
     <demo-block title="圆形">
-      <wd-img src="https://img10.360buyimg.com/img/jfs/t1/105695/24/9139/98608/5e0b0faeE8933b6fa/dcf9596d4a744228.gif"/>
+      <div class="col" v-for="fit in fits">
+        <wd-img round width="100%" height="27vw" :src="joy" :fit="fit" />
+        <div class="center">{{fit}}</div>
+      </div>
     </demo-block>
   </div>
 </template>
 
 <script>
+import Joy from '../assets/img/joy.png'
 export default {
-
+  data () {
+    return {
+      joy: Joy,
+      fits: ['contain', 'cover', 'fill', 'none', 'scale-down']
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-
+  .col {
+    float: left;
+    width: 33.333%;
+    box-sizing: border-box;
+    min-height: 1px;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-bottom: 20px;
+  }
+  .center {
+    text-align: center;
+  }
 </style>
