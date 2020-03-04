79267655af387b1fa0590137827194469e6b264a
renjing36 <renjing10@jd.com>
2020-03-04 10:19:34 +0800

    feat(statusTip): add new component
    feat(statusTip): merge dev resolve conflict
    
    feat(statusTip): remove redundancy demo
    
    feat(statusTip): remove btn show
    
    feat(statusTip): remove slot

---
+++ packages/status-tip/src/main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wd-status-tip">
+    <img class="wd-status-tip__img" :src="imgName">
+    <p v-if="tip && tip.length !== 0" class="wd-status-tip__text">{{tip}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WdStatusTip',
+  props: {
+    type: {
+      type: String,
+      default: 'network'
+    },
+    tip: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    imgName () {
+      const imgFiles = require.context('../img', false, /.svg$/)
+      try {
+        return imgFiles(`./default-${this.type}.svg`)
+      } catch (e) {
+        return imgFiles(`./default-network.svg`)
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
