fd8302189a2bb71049308dfc58c7574a9f6c60da
wangjing <wangjing90@jd.com>
2020-07-28 22:32:12 +0800

    feat(tabbar): tabbar品牌升级
    feat(tabbar): tabbar品牌升级
    
    feat(tabbar): tabbar文档修改
    
    feat(tabbar): tabbar升级
    
    feat(tabbar): tabbar样式调整

--- examples/demo/pages/tabbar.vue
+++ examples/demo/pages/tabbar.vue
@@ -2,36 +2,43 @@
   <div>
     <demo-block title="基本用法" transparent>
       <wd-tabbar v-model="tabbar1">
-        <wd-tabbar-item icon="wd-icon-cart">购物车</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-collect">收藏</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-person">我</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-bags">服务</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-dong">咚咚</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-chat">我</wd-tabbar-item>
       </wd-tabbar>
     </demo-block>
     <demo-block title="显示角标" transparent>
       <wd-tabbar v-model="tabbar2">
-        <wd-tabbar-item icon="wd-icon-cart" dot>购物车</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-collect" value="new">收藏</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-person" :value="7">我</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-bags" dot>服务</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-dong" value="new" :top="3" :right="-8">咚咚</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-chat" :value="7" :top="3">我</wd-tabbar-item>
       </wd-tabbar>
     </demo-block>
     <demo-block title="修改颜色" transparent>
       <wd-tabbar v-model="tabbar3" active-color="#f00" badge-color="#0083ff">
-        <wd-tabbar-item icon="wd-icon-cart" dot>购物车</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-collect">收藏</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-person">我</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-bags" dot>服务</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-dong">咚咚</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-chat">我</wd-tabbar-item>
+      </wd-tabbar>
+    </demo-block>
+    <demo-block title="修改选中icon" transparent>
+      <wd-tabbar v-model="tabbar4">
+        <wd-tabbar-item icon="wd-icon-bags" active-icon="wd-icon-read" dot>服务</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-dong" active-icon="wd-icon-read">咚咚</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-chat" active-icon="wd-icon-read">我</wd-tabbar-item>
       </wd-tabbar>
     </demo-block>
     <demo-block title="路由模式" transparent>
       <wd-tabbar>
-        <wd-tabbar-item icon="wd-icon-cart" to="/tabbar">标签栏</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-collect" to="/button">按钮</wd-tabbar-item>
-        <wd-tabbar-item icon="wd-icon-person" to="https://m.jd.com">京东</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-bags" to="/tabbar">标签栏</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-dong" to="/button">按钮</wd-tabbar-item>
+        <wd-tabbar-item icon="wd-icon-chat" to="https://m.jd.com">京东</wd-tabbar-item>
       </wd-tabbar>
     </demo-block>
-    <wd-tabbar v-model="tabbar4" fixed border>
-      <wd-tabbar-item icon="wd-icon-cart">购物车</wd-tabbar-item>
-      <wd-tabbar-item icon="wd-icon-collect">收藏</wd-tabbar-item>
-      <wd-tabbar-item icon="wd-icon-person">我</wd-tabbar-item>
+    <wd-tabbar v-model="tabbar5" fixed border>
+      <wd-tabbar-item icon="wd-icon-bags">服务</wd-tabbar-item>
+      <wd-tabbar-item icon="wd-icon-dong">咚咚</wd-tabbar-item>
+      <wd-tabbar-item icon="wd-icon-chat">我</wd-tabbar-item>
     </wd-tabbar>
   </div>
 </template>
@@ -43,7 +50,8 @@ export default {
       tabbar1: 0,
       tabbar2: 0,
       tabbar3: 0,
-      tabbar4: 0
+      tabbar4: 0,
+      tabbar5: 0
     }
   }
 }
