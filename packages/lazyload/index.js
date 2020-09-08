ebaa27565940d2179a2fd536740aceb74c0d811a
zhongzhijie <zhongzhijie@jd.com>
2020-09-08 15:44:18 +0800

    feat(lazyLoad): 图片懒加载兜底图 (#196)

--- packages/lazyload/index.js
+++ packages/lazyload/index.js
@@ -1,10 +1,12 @@
 import Lazyload from 'vue-lazyload'
 import lazyLoading from './img/loading.svg'
+import lazyError from './img/error.svg'
 
 export default {
   install (Vue, options) {
     Vue.use(Lazyload, Object.assign({
       loading: lazyLoading,
+      error: lazyError,
       attempt: 3
     }, options))
   }
