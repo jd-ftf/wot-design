2970b0ae723cbad8fb1e409777cfa6ef4830f66d
zhongzhijie <zhongzhijie@jd.com>
2020-09-17 15:24:38 +0800

    fix(search): 修复搜索框无法长按复制和选中光标位置

--- packages/search/src/main.vue
+++ packages/search/src/main.vue
@@ -15,6 +15,7 @@
             @compositionstart="toggleTyping"
             @compositionend="toggleTyping"
             @blur="searchBlur"
+            @focus="searchFocus"
             :disabled="disabled"
             :maxlength="maxlength"
             :autofocus="autofocus"
@@ -82,9 +83,6 @@ export default {
     searchFocus () {
       if (!this.disabled) {
         this.isFocus = true
-        let clickEvent = new Event('touchstart')
-        this.$refs.searchInput.dispatchEvent(clickEvent)
-        this.$refs.searchInput.focus()
       }
       this.$emit('focus')
     },
