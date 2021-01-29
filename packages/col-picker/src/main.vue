f6bd15cc33e12a854025e46f910d902523ff6c81
wangjing <wangjing90@jd.com>
2021-01-30 02:35:35 +0800

    feat(colPicker): 初始化修改

--- packages/col-picker/src/main.vue
+++ packages/col-picker/src/main.vue
@@ -168,14 +168,14 @@ export default {
   },
   watch: {
     value: {
-      handler (val, oldVal) {
+      handler (val) {
         let jumpFlag = false
         val && val.some((value, index) => {
           if (jumpFlag) return true
           this.selectList[index] && this.selectList[index].some((selectItem, selectIndex) => {
-            // 当前 value 项在 selectList 中对应列能找到，及匹配成功，继续匹配下一列
+            // 当前 value 对应项在 columns 中能成功匹配，继续匹配下一项
             if (value === selectItem.value) return true
-            // 直到最后一项 value 也不能在地址列表中匹配，则更新地址列表值
+            // 直到最后一项 value 也不能在 columns 中匹配，则更新 columns 数据
             if (selectIndex === this.selectList[index].length - 1) {
               this.selectList = this.columns.slice(0)
               this.isCompleting = false
